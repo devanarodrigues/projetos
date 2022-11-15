bd7daa7ffeb77a61f10a1a8845c5aa355987fc50
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-11-14 23:55:03 -0300

    estilizacao modal + projeto4

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,12 +1,24 @@
-let blur = document.querySelector("#blur")
-let container = document.querySelector(".wrapper")
-let popup = document.querySelector(".popup")
-
-function active() {
-    container.classList.toggle('blur')
-    popup.style.opacity = '1'
+function toggle(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup')
+    popup.classList.toggle('active')
 }
-function hide() {
-    container.classList.toggle('blur')
-    popup.style.opacity = '0'
+function toggle2(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup2')
+    popup.classList.toggle('active')
+}
+function toggle3(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup3')
+    popup.classList.toggle('active')
+}
+function toggle4(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup4')
+    popup.classList.toggle('active')
 }
