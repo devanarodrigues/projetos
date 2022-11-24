8818dbedc8206a99b0c23f961b9dce2b880097fc
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-11-24 18:08:55 -0300

    ajustes reveal

--- assets/script/reveal.js
+++ assets/script/reveal.js
@@ -3,3 +3,14 @@ ScrollReveal().reveal(".tile", {
   interval: 800,
   reset: true,
 });
+
+// ScrollReveal().reveal('.scaleUp', 
+// { scale: 0.85, 
+//   reset: true
+// });
+
+// if (window.matchMedia("(max-width: 768px)").matches) {
+//   let card = document.getElementsByClassName("card")
+//   card.classList.add('.scaleUp')
+  
+// }
