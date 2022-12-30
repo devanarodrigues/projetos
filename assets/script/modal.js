be949dbc41808294cb35dbe8585287d17d3a56c3
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-12-29 22:32:25 -0300

    projeto hour-simulator adicionado

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,75 +1,59 @@
-function toggle(){
+function toggle() {
     var blur = document.getElementById('blur')
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup')
     popup.classList.toggle('active')
 }
-function toggle2(){
+function toggle2() {
     var blur = document.getElementById('blur')
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup2')
     popup.classList.toggle('active')
 }
-function toggle3(){
+function toggle3() {
     var blur = document.getElementById('blur')
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup3')
     popup.classList.toggle('active')
 }
-function toggle4(){
+function toggle4() {
     var blur = document.getElementById('blur')
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup4')
     popup.classList.toggle('active')
 }
-function toggle5(){
+function toggle5() {
     var blur = document.getElementById('blur')
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup5')
     popup.classList.toggle('active')
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
-// criar function universal para poder fechar qualquer popup
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-const id1 = new toggle(class) // id em string
-=======
-=======
-id1 = new toggle(class) // id em string
->>>>>>> parent of e4785f7 (const id1)
-=======
-const id1 = new toggle() // id em string
->>>>>>> parent of 62b03d0 (toggle(class))
-
-// id1 = new toggle(id) // id em string
->>>>>>> parent of 676bfd4 (teste2 mudanças)
-
-// function toggle(id){
-//  var blur = document.getElementById('blur')
-//     blur.classList.toggle('active')
-//     var popup = document.getElementById(id) // ex. id: "popup1"
-//     popup.classList.toggle('active')
-// }
-
-<<<<<<< HEAD
-const botao1 = document.getElementById("botao1")
-botao1.addEventListener("click", id1(class))
-=======
->>>>>>> parent of c452b34 (função toggle por id)
-=======
-// id1 = new toggle(id)
-
-// function toggle(id){
-//  var blur = document.getElementById('blur')
-//     blur.classList.toggle('active')
-//     var popup = document.getElementById(id)
-//     popup.classList.toggle('active')
-// }
->>>>>>> parent of ed1de55 (anotação de mudanças)
-=======
-// const botao1 = document.getElementById("botao1")
-// botao1.addEventListener("click", id1(id))
->>>>>>> parent of 676bfd4 (teste2 mudanças)
+    function toggle() {
+        var blur = document.getElementById('blur')
+        blur.classList.toggle('active')
+        var popup = document.querySelector('.popup')
+        popup.classList.toggle('active')
+    }
+    function toggle2() {
+        var blur = document.getElementById('blur')
+        blur.classList.toggle('active')
+        var popup = document.querySelector('.popup2')
+        popup.classList.toggle('active')
+    }
+    function toggle3() {
+        var blur = document.getElementById('blur')
+        blur.classList.toggle('active')
+        var popup = document.querySelector('.popup3')
+        popup.classList.toggle('active')
+    }
+    function toggle4() {
+        var blur = document.getElementById('blur')
+        blur.classList.toggle('active')
+        var popup = document.querySelector('.popup4')
+        popup.classList.toggle('active')
+    }
+    function toggle5() {
+        var blur = document.getElementById('blur')
+        blur.classList.toggle('active')
+        var popup = document.querySelector('.popup5')
+        popup.classList.toggle('active')
+    }
