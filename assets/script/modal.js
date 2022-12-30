2e38ca35cb13f3e2e192fc7ed4465eed5eb034e6
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-12-29 22:33:49 -0300

    erro sintaxe

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -27,33 +27,34 @@ function toggle5() {
     blur.classList.toggle('active')
     var popup = document.querySelector('.popup5')
     popup.classList.toggle('active')
-    function toggle() {
-        var blur = document.getElementById('blur')
-        blur.classList.toggle('active')
-        var popup = document.querySelector('.popup')
-        popup.classList.toggle('active')
-    }
-    function toggle2() {
-        var blur = document.getElementById('blur')
-        blur.classList.toggle('active')
-        var popup = document.querySelector('.popup2')
-        popup.classList.toggle('active')
-    }
-    function toggle3() {
-        var blur = document.getElementById('blur')
-        blur.classList.toggle('active')
-        var popup = document.querySelector('.popup3')
-        popup.classList.toggle('active')
-    }
-    function toggle4() {
-        var blur = document.getElementById('blur')
-        blur.classList.toggle('active')
-        var popup = document.querySelector('.popup4')
-        popup.classList.toggle('active')
-    }
-    function toggle5() {
-        var blur = document.getElementById('blur')
-        blur.classList.toggle('active')
-        var popup = document.querySelector('.popup5')
-        popup.classList.toggle('active')
-    }
+}
+function toggle() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup')
+    popup.classList.toggle('active')
+}
+function toggle2() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup2')
+    popup.classList.toggle('active')
+}
+function toggle3() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup3')
+    popup.classList.toggle('active')
+}
+function toggle4() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup4')
+    popup.classList.toggle('active')
+}
+function toggle5() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.querySelector('.popup5')
+    popup.classList.toggle('active')
+}
